dcdf7ae9b7119263bc6d35f90ed0a8b299b5bd16
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-19 15:11:47 +0530

    [Dipesh] modify CalculateFareLogicAdded

---
+++ Automate.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+commitMessage="[Dipesh] modify CalculateFareLogicAdded"
+currentBranchName="UC1CalculateFare"
+newBranchName="UC2MultipleRides"
+echo --------git status---------
+echo
+git status
+echo ---------git add *--------------
+echo
+git add *
+echo ------------git commit -m $commitMessage-----------------
+echo
+git commit -m "$commitMessage"
+echo ------------git push-------------------
+echo
+git remote show origin
+git push -u origin $currentBranchName
+echo ------------git checkout Master-------------------
+echo
+git checkout master
+echo ------------git pull-------------------
+echo
+git merge $currentBranchName
+echo ------------git branch and checkout-------------------
+echo
+git branch $newBranchName
+git checkout $newBranchName
